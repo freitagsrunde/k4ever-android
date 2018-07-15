66982550634c20bda7204b5da1fd87d7380aae7b
Markus Ressel <markus9100@gmail.com>
2018-07-15 23:06:32 +0200

    fancy telegram message(?)

--- travis/build.sh
+++ travis/build.sh
@@ -1,17 +1,51 @@
 #!/usr/bin/env bash
 if [[ "${TRAVIS_BRANCH}" == "dev" ]]; then
   ./gradlew clean testDebug lintDebug assembleDebug --stacktrace
+
+  BASE_URL="https://api.telegram.org/${TELEGRAM_TOKEN}"
   
   # find compiled .apk file
-  RELEASE=$(find ./app/build/outputs/apk/debug -type f -name "k4ever*.apk")
+  APK_FILE=$(find ./app/build/outputs/apk/debug -type f -name "k4ever*.apk")
   # send it file via telegram bot
-  curl \
-  --silent \
-  --form chat_id="${CHAT_ID}" \
-  --form document=@"${RELEASE}" \
-  "https://api.telegram.org/${TELEGRAM_TOKEN}/sendDocument" \
-  >/dev/null 2>&1
+  MESSAGE_ID=$(curl \
+    --silent \
+    --form chat_id="${CHAT_ID}" \
+    --form document=@"${APK_FILE}" \
+    "${BASE_URL}/sendDocument" \
+    2>/dev/null \
+    | jq 'if (.ok == true) then .result.message_id else empty end'.)
   
+  # prepare telegram message to send as reply to the apk file
+  if [["${TRAVIS_PULL_REQUEST}" != "false"]]; then
+    PR_LINK_TEXT = $(cat <<EOF
+[Pull Request](https://github.com/freitagsrunde/k4ever-android/pull/${TRAVIS_PULL_REQUEST})
+EOF
+)
+  fi
+  
+  MESSAGE=$(cat <<EOF
+*Travis Build #${TRAVIS_BUILD_NUMBER} (${TRAVIS_EVENT_TYPE})*
+Test Result: ${TRAVIS_TEST_RESULT}
+
+Commits:
+\`${TRAVIS_COMMIT_RANGE}\`
+
+[Travis Build](https://travis-ci.org/freitagsrunde/k4ever-android/builds/${TRAVIS_BUILD_ID})    ${PR_LINK_TEXT}
+EOF
+)
+
+  # send telegram chat message
+  curl \
+    --silent \
+    -X POST \
+    "${BASE_URL}/sendMessage" \
+    -d "chat_id=${CHAT_ID}" \
+    -d "text=${MESSAGE}" \
+    -d "reply_to_message_id=${MESSAGE_ID}"
+    -d "parse_mode=markdown" \
+    -d "disable_web_page_preview=true"
+
 else
   ./gradlew clean testDebug lintDebug --stacktrace
 fi
+
